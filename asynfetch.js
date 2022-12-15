97355ad99f6ad4bb3db2488b32e3c83ea4e43a5b
Sunday Omolewu <omolewu12@gmail.com>
2022-12-15 11:49:54 +0100

    date class

---
+++ asynfetch.js
@@ -0,0 +1,32 @@
+
+
+//  const  getdata = async (url) =>{
+
+//     let fetchData = await fetch(url);
+//     let jsonData = await fetchData.json()
+//     console.log(jsonData)
+
+// }
+
+// getdata(url)
+
+// fetch(url).then(results => {
+//     return results.json()
+// }).then(data =>{
+    
+//     console.log(data)
+    
+// }).catch(err => comsole.log(err))
+
+async function getEx(){
+    try{
+        let from =
+        let results = await fetch(`https://v6.exchangerate-api.com/v6/cc02bc7972a0c8b71e9ac68b/pair/${}/{}`);
+        let data = await results.json()
+        console.log(data)
+    }catch(err){
+         console.log(err)
+    }
+ 
+}
+getEx()
